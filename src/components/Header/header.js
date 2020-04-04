5c51c3439ed5d6fc43e6d72690210ff75ea2f35c
modeeq <modeeqali1@gmail.com>
2020-04-04 14:12:33 -0400

    add header component

---
+++ src/components/Header/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => {
+	return (
+		<div className='header'>
+			<h1>
+				Food<span id='ex'>Ex</span>
+			</h1>
+		</div>
+	);
+};
+
+export default Header;
