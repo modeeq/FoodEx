777967101c7e4841d6e6540d7a5eb616fc50d6ed
modeeq <modeeqali1@gmail.com>
2020-04-04 21:57:44 -0400

    add restaurant card component

---
+++ src/components/Restaurant-card/restaurant-card.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './restaurant-card.css';
+
+const RestaurantCard = () => {
+	return (
+		<div className='restaurant-card'>
+			<div className='img'>
+				<img src='./images/img2.png' alt='food' />
+			</div>
+			<div className='name'>
+				<p>Atlanta Breakfast Club</p>
+			</div>
+			<div className='rating'>
+				<img src='images/rating.png' alt='rating' />
+			</div>
+			<div className='info'>
+				<p> cafe, Desserts</p>
+				<p id='reviews'>5 reviews</p>
+				<p>open now</p>
+			</div>
+			<div className='action'>
+				<button>Learn More</button>
+				<button>Directions</button>
+			</div>
+			<div className='options'>
+				<p>Delivery</p>
+				<p>Takeaways</p>
+			</div>
+		</div>
+	);
+};
+export default RestaurantCard;
