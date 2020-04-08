ea6bbce47797eb4de7406f2b46e55bd9a6870486
modeeq <modeeqali1@gmail.com>
2020-04-08 16:04:17 -0400

    add page route

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home/home';
+import About from './Pages/Restaurant-info/restaurant-info';
+import Search from './Pages/Search/search';
+import Directions from './Pages/Directions/directions';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<BrowserRouter>
+				<Route exact path='/' component={Home} />
+				<Route path='/Search' component={Search} />
+				<Route path='/About' component={About} />
+				<Route path='/direction' component={Directions} />
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
